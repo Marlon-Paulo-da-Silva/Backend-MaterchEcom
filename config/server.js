ee8b5c5a40a43384946c452bbafce8320d1ad45e
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2021-07-21 11:48:05 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5d076173037c5968e0c5378ddca4525a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8bac0357-d4c8-451e-8338-3916979651f7'
+};
